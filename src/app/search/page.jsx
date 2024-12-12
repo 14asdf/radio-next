0b9215004bf0ceb1026b0b97008b0c4760c1abf5
Alexey <al.suxarew@gmail.com>
2024-12-13 01:50:19 +0300

    chore(app): update page

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,10 +1,11 @@
 import { Suspense } from 'react';
 import StationSearch from '@/components/StationSearch';
+import HomeClient from '@/components/HomeClient';
 
 export default function SearchPage() {
   return (
     <Suspense>
-      <StationSearch />
+      <HomeClient />
     </Suspense>
   );
 }
