bb04e597be05aabac94603f05bbf8a5f8ac0b84d
Alexey <al.suxarew@gmail.com>
2025-01-08 13:58:21 +0300

    chore: update request

--- src/i18n/request.js
+++ src/i18n/request.js
@@ -1,8 +1,13 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ requestLocale }) => {
+  let locale = await requestLocale;
+
+  const messages = (await import(`../../messages/${locale}.json`)).default;
+
   return {
-    locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    messages,
+    locale: locale,
+    timeZone: 'system',
   };
 });
