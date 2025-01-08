908e12702a5e7cd83626ce40d5f3edfce5ae48dd
Alexey <al.suxarew@gmail.com>
2025-01-08 06:06:13 +0300

    chore: update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  modularizeImports: {
-    lodash: {
-      transform: 'lodash/{{member}}',
-    },
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
