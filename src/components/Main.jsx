98098ea0cd44128b726dd49748073f47dac5bac3
Alexey <al.suxarew@gmail.com>
2025-01-10 18:34:36 +0300

    chore: update Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -27,6 +27,7 @@ import { useAudioPlayer } from '../contexts/AudioPlayerContext';
 import MiniPlayer from './MiniPlayer';
 import Logo from './Logo';
 import { useTranslations } from 'next-intl';
+import Script from 'next/script';
 
 const Main = ({ children }) => {
   const t = useTranslations('main.navigation');
@@ -41,6 +42,30 @@ const Main = ({ children }) => {
 
   return (
     <>
+      <Script id="yandex-metrika" strategy="afterInteractive">
+        {`
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+          ym(99481187, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+          });
+        `}
+      </Script>
+      <noscript>
+        <div>
+          <img
+            src="https://mc.yandex.ru/watch/99481187"
+            style={{ position: 'absolute', left: '-9999px' }}
+            alt=""
+          />
+        </div>
+      </noscript>
       <Toaster />
       <Box
         height={{ base: '100dvh', md: '100vh' }}
