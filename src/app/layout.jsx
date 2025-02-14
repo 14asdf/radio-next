e2ab1c5a6e4a5dbe9d6ba554662cbf2b722358b6
Alexey <al.suxarew@gmail.com>
2025-02-14 05:07:04 +0300

    fix(assets): favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,8 +19,29 @@ export const viewport = {
 // Updated metadata configuration
 export const metadata = {
   icons: {
-    icon: '/favicon.ico',
-    apple: '/apple-touch-icon.png',
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-128x128.png', sizes: '128x128', type: 'image/png' },
+      { url: '/favicon-256x256.png', sizes: '256x256', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    shortcut: ['/favicon.ico'],
+    other: [
+      {
+        url: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        url: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
   },
   manifest: '/manifest.json',
 };
@@ -39,6 +60,24 @@ export default async function RootLayout({ children }) {
       <head>
         <meta name="yandex-verification" content="ee7e9085c739e9a7" />
         <meta name="msvalidate.01" content="301D34290CADE681F25EBB82B2E4DBC8" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
       </head>
       <body suppressHydrationWarning>
         <div id="root">
